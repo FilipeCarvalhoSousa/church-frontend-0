6e980ac9cb0cc4d47bc0d1b5dd83975ed072f505
Filipe Carvalho <filipecarvalhosousa@gmail.com>
2024-07-19 22:55:03 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { MembroListComponent } from './pages/membros/membro-list/membro-list.component';
-import { MembroEdicaoComponent } from './pages/membros/membro-edicao/membro-edicao.component';
-import { DepartamentoListaComponent } from './pages/departamentos/departamento-lista/departamento-lista.component';
-import { DepartamentoEdicaoComponent } from './pages/departamentos/departamento-edicao/departamento-edicao.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  {
-    path: 'membros',
-    component: MembroListComponent
-  },
-  {
-    path: 'membros/edit/:id',
-    component: MembroEdicaoComponent
-  },
-  {
-    path: 'membros/add',
-    component: MembroEdicaoComponent
-  },
-  {
-    path: 'departamento',
-    component: DepartamentoListaComponent
-  },
-  {
-    path: 'departamento/add',
-    component: DepartamentoEdicaoComponent
-  },
-  {
-    path: 'departamento/edit/:id',
-    component: DepartamentoEdicaoComponent
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,34 +16,12 @@ import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { MessageBarComponent } from './components/message-bar/message-bar.component';
-import { HomeComponent } from './home/home.component';
-import { MembroDetalheComponent } from './pages/membros/membro-detalhe/membro-detalhe.component';
-import { MembroEdicaoComponent } from './pages/membros/membro-edicao/membro-edicao.component';
-import { MembroListComponent } from './pages/membros/membro-list/membro-list.component';
-import { DepartamentoListaComponent } from './pages/departamentos/departamento-lista/departamento-lista.component';
-import { DepartamentoEdicaoComponent } from './pages/departamentos/departamento-edicao/departamento-edicao.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-    MessageBarComponent,
-    MembroEdicaoComponent,
-    MembroDetalheComponent,
-    MenuBarComponent,
-    MembroListComponent,
-    DepartamentoListaComponent,
-    DepartamentoEdicaoComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
